231379e748d92c321a98534706149752393fb482
Joel Chelliah <joel.chelliah@bekk.no>
2016-11-12 09:40:12 +0100

    Set up basic game architecture

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,44 @@
 import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Time exposing (Time, second)
 
-main : Html a
-main =
-  text "Hello Elm?"
+
+main = App.program { init = init
+                   , view = view
+                   , update = update
+                   , subscriptions = subscriptions
+                   }
+
+-- Model
+
+type alias Model = { rex: String
+                   , obstacles : List String
+                   }
+                   
+init : (Model, Cmd Msg)
+init = (Model "RAWR! (todo)" [], Cmd.none)
+
+
+-- Update
+
+type Msg = Tick Time
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Tick newTime -> (model, Cmd.none)
+
+
+-- Subscriptions
+
+subscriptions : Model -> Sub Msg
+subscriptions model = Time.every second Tick
+
+
+-- View
+
+view : Model -> Html Msg
+view model = div [id "main-container"]
+                 [text (toString model)]
