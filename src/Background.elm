c0c6ec8b312fd07e2a59fc2a3314f61ff129ff78
Joel Chelliah <joel.chelliah@bekk.no>
2017-06-11 09:33:40 +0200

    Refactoring

--- src/Background.elm
+++ src/Background.elm
@@ -1,52 +1,88 @@
 module Background exposing (view)
 
 import WindowSize exposing (..)
-
 import List exposing (map, map2)
+import List.Extra exposing (iterate)
 import Svg exposing (Svg)
 import Svg.Attributes exposing (..)
 
 
 view : Svg {}
 view =
-  Svg.svg [] [ renderSky
-             , renderGround
-             ]
+    Svg.svg []
+        [ renderSky
+        , renderGround
+        ]
 
 
-renderSky: Svg {}
+renderSky : Svg {}
 renderSky =
-  Svg.rect [ fill "#99E"
-           , x "0"
-           , y "0"
-           , width  <| toString windowWidth
-           , height <| toString windowHeight
-           ]
-           []
+    Svg.rect
+        [ fill "#99E"
+        , x "0"
+        , y "0"
+        , width <| toString windowWidth
+        , height <| toString windowHeight
+        ]
+        []
 
 
 renderGround : Svg {}
 renderGround =
-  let filledRows = map fillRect groundRowColors
-      placedRows = map2 identity filledRows groundRowPositions
-  in Svg.svg [] placedRows
+    let
+        filledRows =
+            map fillRect groundRowColors
+
+        placedRows =
+            map2 identity filledRows groundRowPositions
+    in
+        Svg.svg [] placedRows
+
 
 fillRect : String -> Float -> Svg {}
 fillRect col yPos =
-  Svg.rect [ fill col
-           , x "0"
-           , y <| toString <| windowHeight - yPos
-           , width  <| toString windowWidth
-           , height <| toString yPos
-           ]
-           []
+    Svg.rect
+        [ fill col
+        , x "0"
+        , y <| toString <| windowHeight - yPos
+        , width <| toString windowWidth
+        , height <| toString yPos
+        ]
+        []
+
 
 groundRowColors : List String
 groundRowColors =
-  let line = "#EEC39A"
-  in line :: ["#D9A066", "#CE975F", "#C18C57", "#B58452", "#A87A4C", "#9A7046"]
+    let
+        topBorder =
+            "#EEC39A"
+    in
+        topBorder
+            :: [ "#D9A066"
+               , "#CE975F"
+               , "#C18C57"
+               , "#B58452"
+               , "#A87A4C"
+               , "#9A7046"
+               , "#8C6545"
+               , "#825E41"
+               , "#78563C"
+               ]
+
 
 groundRowPositions : List Float
 groundRowPositions =
-  let line = 90
-  in line :: [88, 72, 56, 40, 24, 8]
+    let
+        topBorder =
+            90
+
+        gradientH =
+            10
+
+        nextLine h =
+            if h < gradientH then
+                Nothing
+            else
+                Just (h - gradientH)
+    in
+        topBorder :: iterate nextLine 88
--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
+module Main exposing (..)
+
 import Rex
 import CactusGenerator as CactusGen
 import DirtGenerator as DirtGen
 import MovingElement as Elem
 import Background
 import WindowSize exposing (..)
-
 import Html exposing (Html, programWithFlags, div, map)
 import Time exposing (Time)
 import Keyboard exposing (KeyCode)
@@ -13,152 +14,224 @@ import Svg.Attributes exposing (..)
 import AnimationFrame
 import Random exposing (initialSeed)
 
+
 main : Program Flags Model Msg
-main = programWithFlags { init = init
-                        , view = view
-                        , update = update
-                        , subscriptions = subscriptions
-                        }
+main =
+    programWithFlags
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- Model
 
-type GameState = New
-               | Playing
-               | Paused
-               | End
 
-type alias Model = { state: GameState
-                   , rex: Rex.Model
-                   , cactusGen: CactusGen.Model
-                   , dirtGen: DirtGen.Model
-                   }
+type GameState
+    = New
+    | Playing
+    | Paused
+    | End
 
-type alias Flags = { randomSeed : Int }
 
-init : Flags -> (Model, Cmd Msg)
-init {randomSeed} =
-  let seed0 = initialSeed randomSeed
-      model = { state     = New
-              , rex       = Rex.init
-              , cactusGen = CactusGen.init windowWidth seed0
-              , dirtGen   = DirtGen.init windowWidth
-              }
-  in (model, Cmd.none)
+type alias Model =
+    { state : GameState
+    , rex : Rex.Model
+    , cactusGen : CactusGen.Model
+    , dirtGen : DirtGen.Model
+    }
+
+
+type alias Flags =
+    { randomSeed : Int }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init { randomSeed } =
+    let
+        model =
+            { state = New
+            , rex = Rex.init
+            , cactusGen = CactusGen.init windowWidth <| initialSeed randomSeed
+            , dirtGen = DirtGen.init windowWidth
+            }
+    in
+        ( model, Cmd.none )
+
 
 
 -- Update
 
-type Msg = Tick Time
-         | KeyPressed KeyCode
-         | KeyReleased
-         | SubMsg
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type Msg
+    = Tick Time
+    | KeyPressed KeyCode
+    | KeyReleased
+    | SubMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case model.state of
-    Playing -> (updatePlaying msg model, Cmd.none)
-    _       -> (updatePaused msg model, Cmd.none)
+    case model.state of
+        Playing ->
+            ( updatePlaying msg model, Cmd.none )
+
+        _ ->
+            ( updatePaused msg model, Cmd.none )
+
 
 updatePaused : Msg -> Model -> Model
 updatePaused msg model =
-  case msg of
-    KeyPressed 32 -> { model | state = Playing }
-    _ ->             model
+    case msg of
+        KeyPressed 32 ->
+            { model | state = Playing }
+
+        _ ->
+            model
+
 
 updatePlaying : Msg -> Model -> Model
 updatePlaying msg model =
-  case msg of
-    KeyPressed 32 ->
-        { model | state = Paused }
-    KeyPressed code ->
-        { model | rex = Rex.update (codeToMsg code) model.rex }
-    KeyReleased ->
-      { model | rex = Rex.update Rex.Run model.rex }
-    Tick delta ->
-      { model | rex = Rex.update (Rex.Tick delta) model.rex
-              , cactusGen = CactusGen.update delta model.cactusGen
-              , dirtGen = DirtGen.update delta model.dirtGen }
-    SubMsg ->
-      model
+    case msg of
+        KeyPressed 32 ->
+            { model | state = Paused }
+
+        KeyPressed code ->
+            { model | rex = Rex.update (codeToMsg code) model.rex }
+
+        KeyReleased ->
+            { model | rex = Rex.update Rex.Run model.rex }
+
+        Tick delta ->
+            { model
+                | rex = Rex.update (Rex.Tick delta) model.rex
+                , cactusGen = CactusGen.update delta model.cactusGen
+                , dirtGen = DirtGen.update delta model.dirtGen
+            }
+
+        SubMsg ->
+            model
+
 
 codeToMsg : KeyCode -> Rex.Msg
 codeToMsg code =
-  case code of
-    40 -> Rex.Duck
-    38 -> Rex.Jump
-    _  -> Rex.Run
+    case code of
+        40 ->
+            Rex.Duck
+
+        38 ->
+            Rex.Jump
+
+        _ ->
+            Rex.Run
+
 
 
 -- Subscriptions
 
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.batch [ AnimationFrame.diffs Tick
-            , Keyboard.downs KeyPressed
-            , Keyboard.ups (\_ -> KeyReleased)
-            ]
+    Sub.batch
+        [ AnimationFrame.diffs Tick
+        , Keyboard.downs KeyPressed
+        , Keyboard.ups (\_ -> KeyReleased)
+        ]
+
 
 
 -- View
 
-view : Model -> Html Msg
-view ({state, rex, cactusGen, dirtGen} as model) =
-  let (w, h) = (toString windowWidth, toString windowHeight)
-      attributes = [ width w
-                   , height h
-                   , viewBox <| "0 0 " ++ w ++ " " ++ h
-                   , version "1.1"
-                   , style   "position: fixed;"
-                   ]
-      sceneElements = [ renderBackground
-                      , renderMovingElements cactusGen.cacti
-                      , renderRex rex
-                      , renderMessages state
-                      ]
 
-  in  Svg.svg attributes sceneElements
+view : Model -> Html Msg
+view ({ state, rex, cactusGen, dirtGen } as model) =
+    let
+        ( w, h ) =
+            ( toString windowWidth, toString windowHeight )
+
+        attributes =
+            [ width w
+            , height h
+            , viewBox <| "0 0 " ++ w ++ " " ++ h
+            , version "1.1"
+            , style "position: fixed;"
+            ]
+
+        sceneElements =
+            [ renderBackground
+            , renderMovingElements cactusGen.cacti
+            , renderRex rex
+            , renderMessages state
+            ]
+    in
+        Svg.svg attributes sceneElements
 
 
 renderMessages : GameState -> Svg Msg
 renderMessages state =
-  let yMiddle  = windowHeight / 2
-      attrBase = [ x << toString <| windowWidth / 2
-                 , textAnchor "middle"
-                 , fill "#C12"
-                 ]
-      attrLarge yPos = [ y << toString <| yMiddle + yPos
-                       , fontSize "60"
-                       ] ++ attrBase
-      attrSmall yPos = [ y << toString <| yMiddle + yPos
-                       , fontSize "18"
-                       ] ++ attrBase
-  in case state of
-    New ->
-      Svg.svg [] [ Svg.text_ (attrLarge -50) [ Svg.text "RAWЯ!" ]
-                 , Svg.text_ (attrSmall  0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
-                 , Svg.text_ (attrSmall  35) [ Svg.text "Press SPACE to pause" ]
-                 ]
-    Paused ->
-      Svg.svg [] [ Svg.text_ (attrLarge -20) [ Svg.text "Paused!" ]
-                 , Svg.text_ (attrSmall  15) [ Svg.text "Press SPACE to continue" ]
-                 ]
-    End ->
-      Svg.svg [] [ Svg.text_ (attrLarge -20) [ Svg.text "Game Ovər!" ]
-                 , Svg.text_ (attrSmall  15) [ Svg.text "Press SPACE to try again" ]
-                 ]
-    Playing ->
-      Svg.svg [][]
+    let
+        yMiddle =
+            windowHeight / 2
+
+        attrBase =
+            [ x << toString <| windowWidth / 2
+            , textAnchor "middle"
+            , fill "#C12"
+            ]
+
+        attrLarge yPos =
+            [ y << toString <| yMiddle + yPos
+            , fontSize "60"
+            ]
+                ++ attrBase
+
+        attrSmall yPos =
+            [ y << toString <| yMiddle + yPos
+            , fontSize "18"
+            ]
+                ++ attrBase
+    in
+        case state of
+            New ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -50) [ Svg.text "RAWЯ!" ]
+                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
+                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to pause" ]
+                    ]
+
+            Paused ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -50) [ Svg.text "Paused!" ]
+                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
+                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to continue" ]
+                    ]
+
+            End ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -20) [ Svg.text "Game Ovər!" ]
+                    , Svg.text_ (attrSmall 15) [ Svg.text "Press SPACE to try again" ]
+                    ]
+
+            Playing ->
+                Svg.svg [] []
 
 
 renderBackground : Svg Msg
 renderBackground =
-  map (\_ -> SubMsg) Background.view
+    map (\_ -> SubMsg) Background.view
+
 
 renderMovingElements : List (Elem.Model a) -> Svg Msg
 renderMovingElements elems =
-  let render elem = map (\_ -> SubMsg) (Elem.view elem)
-  in Svg.svg [] <| List.map render elems
+    let
+        render elem =
+            map (\_ -> SubMsg) (Elem.view elem)
+    in
+        Svg.svg [] <| List.map render elems
+
 
 renderRex : Rex.Model -> Svg Msg
 renderRex rex =
-  map (\_ -> SubMsg) (Rex.view rex)
+    map (\_ -> SubMsg) (Rex.view rex)
