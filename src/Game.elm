5cd09548ff85dd89062f335106c850824a74e7e3
Joel Chelliah <joel.chelliah@bekk.no>
2017-06-11 19:43:36 +0200

    Seperated main and game related code

---
+++ src/Game.elm
@@ -0,0 +1,250 @@
+module Game exposing (..)
+
+import Hud
+import Rex
+import CactusGenerator as CactusGen
+import MovingElement as Elem
+import Background
+import WindowSize exposing (..)
+import Html exposing (Html, programWithFlags, h1, h5, div, map, a, text)
+import Time exposing (Time)
+import Keyboard exposing (KeyCode)
+import Svg exposing (Svg)
+import Svg.Attributes exposing (..)
+import AnimationFrame
+import Random exposing (Seed)
+
+
+-- Model
+
+
+type GameState
+    = New
+    | Playing
+    | Paused
+    | GameOver
+
+
+type alias Model =
+    { state : GameState
+    , hud : Hud.Model
+    , rex : Rex.Model
+    , cactusGen : CactusGen.Model
+    , seed : Seed
+    }
+
+
+init : Seed -> Model
+init seed =
+    { state = New
+    , hud = Hud.init
+    , rex = Rex.init
+    , cactusGen = CactusGen.init windowWidth seed
+    , seed = seed
+    }
+
+
+
+-- Update
+
+
+type Msg
+    = Tick Time
+    | KeyPressed KeyCode
+    | KeyReleased
+    | SubMsg
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case model.state of
+        Playing ->
+            updatePlaying msg model
+
+        GameOver ->
+            updateGameOver msg model
+
+        _ ->
+            updatePaused msg model
+
+
+updatePlaying : Msg -> Model -> Model
+updatePlaying msg ({ hud, rex, cactusGen } as model) =
+    if (spacePressed msg) then
+        { model | state = Paused }
+    else
+        case msg of
+            KeyPressed code ->
+                { model | rex = Rex.update (codeToRexMsg code) rex }
+
+            KeyReleased ->
+                { model | rex = Rex.update Rex.Run rex }
+
+            Tick delta ->
+                if Rex.hitDetected rex cactusGen.cacti then
+                    { model
+                        | state = GameOver
+                        , hud = Hud.update Hud.Highlight hud
+                        , rex = Rex.update Rex.Kill rex
+                    }
+                else
+                    { model
+                        | rex = Rex.update (Rex.Tick delta) rex
+                        , cactusGen = CactusGen.update delta cactusGen
+                        , hud =
+                            if (Rex.hasLanded rex) then
+                                Hud.update Hud.IncScore hud
+                            else
+                                hud
+                    }
+
+            SubMsg ->
+                model
+
+
+updatePaused : Msg -> Model -> Model
+updatePaused msg model =
+    if (spacePressed msg) then
+        { model | state = Playing }
+    else
+        model
+
+
+updateGameOver : Msg -> Model -> Model
+updateGameOver msg game =
+    if (spacePressed msg) then
+        init game.seed
+    else
+        game
+
+
+codeToRexMsg : KeyCode -> Rex.Msg
+codeToRexMsg code =
+    case code of
+        40 ->
+            Rex.Duck
+
+        38 ->
+            Rex.Jump
+
+        _ ->
+            Rex.Run
+
+
+spacePressed : Msg -> Bool
+spacePressed msg =
+    msg == KeyPressed 32
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch
+        [ AnimationFrame.diffs Tick
+        , Keyboard.downs KeyPressed
+        , Keyboard.ups (\_ -> KeyReleased)
+        ]
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view { state, hud, rex, cactusGen } =
+    let
+        ( w, h ) =
+            ( toString windowWidth, toString windowHeight )
+
+        attributes =
+            [ width w
+            , height h
+            , viewBox <| "0 0 " ++ w ++ " " ++ h
+            , version "1.1"
+            ]
+
+        sceneElements =
+            [ viewBackground
+            , viewMovingElements cactusGen.cacti
+            , viewRex rex
+            , viewAlert state hud.score
+            , viewHud hud
+            ]
+    in
+        Svg.svg attributes sceneElements
+
+
+viewAlert : GameState -> Int -> Svg Msg
+viewAlert state score =
+    let
+        yMiddle =
+            windowHeight / 2
+
+        attrBase =
+            [ x << toString <| windowWidth / 2
+            , textAnchor "middle"
+            , fill "#C12"
+            ]
+
+        attrLarge yPos =
+            [ y << toString <| yMiddle + yPos
+            , fontSize "60"
+            ]
+                ++ attrBase
+
+        attrSmall yPos =
+            [ y << toString <| yMiddle + yPos
+            , fontSize "18"
+            ]
+                ++ attrBase
+    in
+        case state of
+            New ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -50) [ Svg.text "RAWЯ!" ]
+                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
+                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to start / pause" ]
+                    ]
+
+            Paused ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -50) [ Svg.text "Paused!" ]
+                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
+                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to continue" ]
+                    ]
+
+            GameOver ->
+                Svg.svg []
+                    [ Svg.text_ (attrLarge -40) [ Svg.text "Game Ovər!" ]
+                    , Svg.text_ (attrSmall 25) [ Svg.text "Press SPACE to try again" ]
+                    ]
+
+            Playing ->
+                Svg.svg [] []
+
+
+viewBackground : Svg Msg
+viewBackground =
+    map (\_ -> SubMsg) Background.view
+
+
+viewMovingElements : List (Elem.Model a) -> Svg Msg
+viewMovingElements elems =
+    let
+        render elem =
+            map (\_ -> SubMsg) (Elem.view elem)
+    in
+        Svg.svg [] <| List.map render elems
+
+
+viewRex : Rex.Model -> Svg Msg
+viewRex rex =
+    map (\_ -> SubMsg) (Rex.view rex)
+
+
+viewHud : Hud.Model -> Svg Msg
+viewHud hud =
+    map (\_ -> SubMsg) (Hud.view hud)
--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,20 @@
 module Main exposing (..)
 
-import Hud
-import Rex
-import CactusGenerator as CactusGen
-import MovingElement as Elem
-import Background
-import WindowSize exposing (..)
+import Game exposing (Msg)
 import Html exposing (Html, programWithFlags, h1, h5, div, map, a, text)
 import Html.Attributes exposing (href)
-import Time exposing (Time)
-import Keyboard exposing (KeyCode)
-import Svg exposing (Svg)
 import Svg.Attributes exposing (..)
-import AnimationFrame
-import Random exposing (initialSeed)
+import Random exposing (Seed, initialSeed)
 import FontAwesome as Icon
 import Color
+
+
+type alias Model =
+    { game : Game.Model }
+
+
+type alias Flags =
+    { randomSeed : Int }
 
 
 main : Program Flags Model Msg
@@ -28,183 +27,36 @@ main =
         }
 
 
-
--- Model
-
-
-type GameState
-    = New
-    | Playing
-    | Paused
-    | GameOver
-
-
-type alias Model =
-    { state : GameState
-    , hud : Hud.Model
-    , rex : Rex.Model
-    , cactusGen : CactusGen.Model
-    , seed : Int
-    }
-
-
-type alias Flags =
-    { randomSeed : Int }
-
-
 init : Flags -> ( Model, Cmd Msg )
 init { randomSeed } =
     let
-        model =
-            { state = New
-            , hud = Hud.init
-            , rex = Rex.init
-            , cactusGen = CactusGen.init windowWidth <| initialSeed randomSeed
-            , seed = randomSeed
-            }
+        seed =
+            initialSeed randomSeed
     in
-        ( model, Cmd.none )
-
-
-
--- Update
-
-
-type Msg
-    = Tick Time
-    | KeyPressed KeyCode
-    | KeyReleased
-    | SubMsg
+        { game = Game.init seed } ! [ Cmd.none ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case model.state of
-        Playing ->
-            ( updatePlaying msg model, Cmd.none )
-
-        GameOver ->
-            ( updateGameOver msg model, Cmd.none )
-
-        _ ->
-            ( updatePaused msg model, Cmd.none )
-
-
-updatePlaying : Msg -> Model -> Model
-updatePlaying msg ({ hud, rex, cactusGen } as model) =
-    if (spacePressed msg) then
-        { model | state = Paused }
-    else
-        case msg of
-            KeyPressed code ->
-                { model | rex = Rex.update (codeToRexMsg code) rex }
-
-            KeyReleased ->
-                { model | rex = Rex.update Rex.Run rex }
-
-            Tick delta ->
-                if Rex.hitDetected rex cactusGen.cacti then
-                    { model
-                        | state = GameOver
-                        , hud = Hud.update Hud.Highlight hud
-                        , rex = Rex.update Rex.Kill rex
-                    }
-                else
-                    { model
-                        | rex = Rex.update (Rex.Tick delta) rex
-                        , cactusGen = CactusGen.update delta cactusGen
-                        , hud =
-                            if (Rex.hasLanded rex) then
-                                Hud.update Hud.IncScore hud
-                            else
-                                hud
-                    }
-
-            SubMsg ->
-                model
-
-
-updatePaused : Msg -> Model -> Model
-updatePaused msg model =
-    if (spacePressed msg) then
-        { model | state = Playing }
-    else
-        model
-
-
-updateGameOver : Msg -> Model -> Model
-updateGameOver msg model =
-    if (spacePressed msg) then
-        Tuple.first <| init { randomSeed = model.seed }
-    else
-        model
-
-
-codeToRexMsg : KeyCode -> Rex.Msg
-codeToRexMsg code =
-    case code of
-        40 ->
-            Rex.Duck
-
-        38 ->
-            Rex.Jump
-
-        _ ->
-            Rex.Run
-
-
-spacePressed : Msg -> Bool
-spacePressed msg =
-    msg == KeyPressed 32
-
-
-
--- Subscriptions
+    { model | game = Game.update msg model.game } ! [ Cmd.none ]
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.batch
-        [ AnimationFrame.diffs Tick
-        , Keyboard.downs KeyPressed
-        , Keyboard.ups (\_ -> KeyReleased)
-        ]
-
-
-
--- View
+subscriptions { game } =
+    Game.subscriptions game
 
 
 view : Model -> Html Msg
-view ({ state, hud, rex, cactusGen } as model) =
-    let
-        ( w, h ) =
-            ( toString windowWidth, toString windowHeight )
-
-        attributes =
-            [ width w
-            , height h
-            , viewBox <| "0 0 " ++ w ++ " " ++ h
-            , version "1.1"
-            ]
-
-        sceneElements =
-            [ renderBackground
-            , renderMovingElements cactusGen.cacti
-            , renderRex rex
-            , renderMessages state hud.score
-            , renderHud hud
-            ]
-    in
-        div []
-            [ renderHeader
-            , Svg.svg attributes sceneElements
-            , renderInfo
-            ]
+view ({ game } as model) =
+    div []
+        [ viewHeader
+        , Game.view game
+        , viewInfo
+        ]
 
 
-renderHeader : Html Msg
-renderHeader =
+viewHeader : Html Msg
+viewHeader =
     div []
         [ h1 []
             [ text "ELM-REX" ]
@@ -212,87 +64,14 @@ renderHeader =
         ]
 
 
-renderMessages : GameState -> Int -> Svg Msg
-renderMessages state score =
-    let
-        yMiddle =
-            windowHeight / 2
-
-        attrBase =
-            [ x << toString <| windowWidth / 2
-            , textAnchor "middle"
-            , fill "#C12"
-            ]
-
-        attrLarge yPos =
-            [ y << toString <| yMiddle + yPos
-            , fontSize "60"
-            ]
-                ++ attrBase
-
-        attrSmall yPos =
-            [ y << toString <| yMiddle + yPos
-            , fontSize "18"
-            ]
-                ++ attrBase
-    in
-        case state of
-            New ->
-                Svg.svg []
-                    [ Svg.text_ (attrLarge -50) [ Svg.text "RAWЯ!" ]
-                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
-                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to start / pause" ]
-                    ]
-
-            Paused ->
-                Svg.svg []
-                    [ Svg.text_ (attrLarge -50) [ Svg.text "Paused!" ]
-                    , Svg.text_ (attrSmall 0) [ Svg.text "Play using the arrow keys: ↑ ↓" ]
-                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to continue" ]
-                    ]
-
-            GameOver ->
-                Svg.svg []
-                    [ Svg.text_ (attrLarge -50) [ Svg.text "Game Ovər!" ]
-                    , Svg.text_ (attrSmall 35) [ Svg.text "Press SPACE to try again" ]
-                    ]
-
-            Playing ->
-                Svg.svg [] []
-
-
-renderBackground : Svg Msg
-renderBackground =
-    map (\_ -> SubMsg) Background.view
-
-
-renderMovingElements : List (Elem.Model a) -> Svg Msg
-renderMovingElements elems =
-    let
-        render elem =
-            map (\_ -> SubMsg) (Elem.view elem)
-    in
-        Svg.svg [] <| List.map render elems
-
-
-renderRex : Rex.Model -> Svg Msg
-renderRex rex =
-    map (\_ -> SubMsg) (Rex.view rex)
-
-
-renderHud : Hud.Model -> Svg Msg
-renderHud hud =
-    map (\_ -> SubMsg) (Hud.view hud)
-
-
-renderInfo : Html Msg
-renderInfo =
+viewInfo : Html Msg
+viewInfo =
     let
         url =
             "https://github.com/joelchelliah/elm-rex"
     in
         div
             [ class "info" ]
-            [ Icon.github (Color.black) 20
+            [ Icon.github (Color.black) 30
             , a [ href url ] [ text "Check it out on github" ]
             ]
